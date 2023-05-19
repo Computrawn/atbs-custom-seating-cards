c63d5124e3c4ca8522cd3d163d16a32363634c03
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-05-19 13:23:55 -0500

    Initial commit.

---
+++ custom_seating_cards.py
@@ -0,0 +1,12 @@
+#! python3
+# custom_seating_cards.py — An exercise in using Pillow to manipulate images.
+# For more information, see project_details.txt.
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="logging.txt",
+    format="%(asctime)s -  %(levelname)s -  %(message)s",
+)
+logging.disable(logging.CRITICAL)  # Note out to enable logging.
